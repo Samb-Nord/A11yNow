1959cc7fb128c2c1d43f2e3da8d5802a81e68cce
SamSam <sasa@sasa.fr>
2022-01-05 11:15:53 +0100

    ajout des aria au niveau des champs formulaire, ajout des title et icone

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.onload = function () {
     let form = document.getElementById("formulaire");
     let down = document.getElementById("triangle-down");
     let up = document.getElementById("triangle-up");
+    let send = document.getElementById("send");
 
 
     down.addEventListener("click", () => {
@@ -16,5 +17,34 @@ window.onload = function () {
         down.style.display = "block";
         up.style.display = "none";
     })
+
+    send.addEventListener("click", () => {
+        if (document.form.nom.value != "" && document.form.nom.value != "") {
+            alert("yes");
+        } else {
+            alert("le champs nom est requis");
+        }
+
+        if (!document.form.travail.checked) {
+            alert("yes!")
+        }
+    })
+
+
 }
 
+
+function isChampsNotEmpty() {
+    if (document.form.nom.value != "" && document.form.prenom.value != "" && document.form.mail.value != "" && document.form.site.value != "") {
+        if (document.form.details.value != "") {
+
+        }
+    }
+}
+
+function isOneCheckboxChecked() {
+    if (document.form.travail.checked || document.form.loisirs.checked || document.form.quotidien.checked || document.form.autre.checked) {
+
+    }
+
+}
